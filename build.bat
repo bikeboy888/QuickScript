9455abe5bebb31346b89d25dcbcdccfc08372462
Stephen Quan <squan888@hotmail.com>
2015-12-11 19:39:17 +0000

    Don't generated PDB

--- build.bat
+++ build.bat
@@ -3,8 +3,9 @@
 SETLOCAL
 PATH=%PATH%;C:\Program Files (x86)\Microsoft Visual Studio 9.0\Common7\IDE
 
+RMDIR /s /q Output
 devenv.com QuickScript.sln /build "Release|Pocket PC 2003 (ARMV4)"
+devenv.com QuickScript.sln /build "Release|Windows Mobile 5.0 Pocket PC SDK (ARMV4I)"
 
 DIR "Output\Release\Pocket PC 2003 (ARMV4)"
-
-REM devenv.exe QuickScript.sln /build /project "Release|Pocket PC 2003 (ARMV4)"
+DIR "Output\Release\Windows Mobile 5.0 Pocket PC SDK (ARMV4I)"
